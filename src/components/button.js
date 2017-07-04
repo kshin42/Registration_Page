9455b26af46c4d65d12c6c3d01389f6a7ae505f2
kshin42 <kshin42@gatech.edu>
2017-07-04 18:34:08 -0400

    Clear form when successfully submitted

--- src/components/button.js
+++ src/components/button.js
@@ -12,6 +12,25 @@ var Button = React.createClass ({
 
   _submitForm: function() {
     var validated = validationService();
+    if (validated) {
+      alert("Congratulations! You Have Successfully Registered You Company! ");
+      this._clearForm();
+    }
+
+  },
+
+  _clearForm: function() {
+    $("[id='Company Name']").val("");
+    $("[id='Username']").val("");
+    $("[id='New Password']").val("");
+    $("[id='Validate Password']").val("");
+    $("[id='Email']").val("");
+
+    $("[id='Company Name']").removeClass();;
+    $("[id='Username']").removeClass();
+    $("[id='New Password']").removeClass();
+    $("[id='Validate Password']").removeClass();
+    $("[id='Email']").removeClass();
   }
 });
 
